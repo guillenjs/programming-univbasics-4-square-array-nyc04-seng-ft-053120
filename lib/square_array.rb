eb4c6823dc195526785d4c3176a3834d7fd5b808
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-08 21:44:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   count = 0
   while count < array.size do
     squared << (array[count] ** 2)
-    #count becomes the index number so whatever value is there will be squared
+    #count becomes the index number so whatever integer is at that location there will be squared
     count = count + 1
   end
   squared
