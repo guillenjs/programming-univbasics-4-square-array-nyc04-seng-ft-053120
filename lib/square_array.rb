f5418474253eb200d13509365a1891ebf0cd74a2
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-08 21:44:39 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,10 @@
 def square_array(array)
-  new_array = []
-
-  array.length.times do |index|
-    new_array.push(array[index] ** 2)
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    #count becomes the index number so whatever value is there will be squared
+    count = count + 1
   end
-  new_array
+  squared
 end
